32c79a0b312c17e66f0e2b04b260bd6aec49de10
aoki0430 <pankun0714@gmail.com>
2023-07-29 18:17:15 +0900

    implement upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
+
 <h1 class="text-3xl font-bold underline">Welcome to RaidCast!!</h1>
-<p>Visit<a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+    let files: FileList | null = null;
+  
+    function handleFileChange(event: Event) {
+      const input = event.target as HTMLInputElement;
+      if (input.files) {
+        files = input.files;
+      }
+    }
+  
+    async function uploadFile() {
+      if (!files || files.length === 0) return;
+  
+      const file = files[0];  // 最初のファイルを取得
+      const formData = new FormData();
+      formData.append("file", file);
+  
+      try {
+        const response = await fetch("/upload", {
+          method: "POST",
+          body: formData
+        });
+  
+        if (response.ok) {
+          console.log("File uploaded successfully");
+        } else {
+          console.error("Error uploading file:", response.statusText);
+        }
+      } catch (err) {
+        console.error("Upload failed:", err);
+      }
+    }
+  </script>
+  
+  <input type="file" bind:files={files} on:change={handleFileChange} />
+  <h1>{files}</h1>
+  <button on:click={uploadFile}>Upload</button>
+  
+  
+<div class="h-screen font-sans text-gray-900 bg-gray-300 border-box">
+    <div class="flex justify-center w-full mx-auto sm:max-w-lg">
+
+        <div class="flex flex-col items-center justify-center w-full h-auto my-20 bg-white sm:w-3/4 sm:rounded-lg sm:shadow-xl">
+            <div class="mt-10 mb-10 text-center">
+                <h2 class="text-2xl font-semibold mb-2">Upload your files</h2>
+                <p class="text-xs text-gray-500">File should be of format .mp4, .avi, .mov or .mkv</p>
+            </div>
+            <form action="#" class="relative w-4/5 h-32 max-w-xs mb-10 bg-white bg-gray-100 rounded-lg shadow-inner">
+                <input type="file" bind:files={files} id="file-upload" class="hidden" on:change={handleFileChange}>
+                <label for="file-upload" class="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer">
+                    <p class="z-10 text-xs font-light text-center text-gray-500">Drag & Drop your files here <br><br> or <br><br> Click here and choose file</p>
+
+                    <svg class="z-10 w-8 h-8 text-indigo-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
+                    </svg>
+                </label>
+            </form>
+        </div>
+    </div>
+</div>
+
